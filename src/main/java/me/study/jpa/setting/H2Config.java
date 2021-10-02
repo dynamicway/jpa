ec09b053c921c6b347153f6a0eb3fad6ca933e33
bee <rkddbtjd123@gmail.com>
2021-10-02 23:30:59 +0900

    feat: Add h2 setting what use to tcp option

---
+++ src/main/java/me/study/jpa/setting/H2Config.java
@@ -0,0 +1,21 @@
+package me.study.jpa.setting;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.h2.tools.Server;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class H2Config {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource dataSource() throws SQLException {
+        Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8089").start();
+        return new HikariDataSource();
+    }
+}
