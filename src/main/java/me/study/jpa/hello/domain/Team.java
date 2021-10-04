2e107d61427a945ecae474200d26d5ee66ed4faf
bee <rkddbtjd123@gmail.com>
2021-10-04 23:56:26 +0900

    feat: Add Team Entity

---
+++ src/main/java/me/study/jpa/hello/domain/Team.java
@@ -0,0 +1,19 @@
+package me.study.jpa.hello.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
