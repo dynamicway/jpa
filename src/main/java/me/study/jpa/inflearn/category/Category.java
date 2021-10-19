ae90ea20461802c7f47fad30aebb2ba7bb844d16
bee <rkddbtjd123@gmail.com>
2021-10-19 23:05:16 +0900

    feat: Add Category entity

---
+++ src/main/java/me/study/jpa/inflearn/category/Category.java
@@ -0,0 +1,16 @@
+package me.study.jpa.inflearn.category;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+}
