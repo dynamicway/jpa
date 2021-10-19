d99caf873878ba6e040d9af77996ee5042cfedd9
bee <rkddbtjd123@gmail.com>
2021-10-19 23:28:56 +0900

    feat: Add orderItems field to Order entity

--- src/main/java/me/study/jpa/inflearn/order/Order.java
+++ src/main/java/me/study/jpa/inflearn/order/Order.java
@@ -5,11 +5,14 @@ import me.study.jpa.inflearn.member.Member;
 
 import javax.persistence.*;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
 @Getter
 public class Order {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,6 +20,9 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private ZonedDateTime orderDate;
 
