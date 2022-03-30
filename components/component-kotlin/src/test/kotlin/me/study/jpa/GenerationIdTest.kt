8d7f6abea683eadee279ff43fa6314285c95d948
bee <rkddbtjd123@gmail.com>
2022-03-30 16:29:42 +0900

    EntityManager::persist 메서드가 트랜잭션이 없더라도 어떤 예외도 발생시키지 않는 테스트 추가

--- components/component-kotlin/src/test/kotlin/me/study/jpa/GenerationIdTest.kt
+++ components/component-kotlin/src/test/kotlin/me/study/jpa/GenerationIdTest.kt
@@ -1,9 +1,12 @@
 package me.study.jpa
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatCode
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import javax.persistence.EntityManager
 import javax.persistence.EntityManagerFactory
 
 @DataJpaTest
@@ -11,9 +14,15 @@ internal class GenerationIdTest @Autowired constructor(
     private val entityManagerFactory: EntityManagerFactory
 ) {
 
+    private lateinit var entityManager: EntityManager
+
+    @BeforeEach
+    internal fun setUp() {
+        entityManager = entityManagerFactory.createEntityManager()
+    }
+
     @Test
-    fun getId_withReflection_when_callsPersist_before_transaction_committed() {
-        val entityManager = entityManagerFactory.createEntityManager()
+    fun getId_with_reflection_when_calls_Persist_before_transaction_committed() {
         val order = Order()
         entityManager.transaction.begin()
         entityManager.persist(order)
@@ -22,6 +31,17 @@ internal class GenerationIdTest @Autowired constructor(
 
         entityManager.transaction.commit()
         entityManager.close()
+    }
+
+    @Test
+    fun persist_without_transaction_not_throw_any_exception() {
+        val order = Order()
+
+        assertThatCode { entityManager.persist(order) }
+            .doesNotThrowAnyException()
+
+        assertThat(order.id).isNull()
+        assertThat(entityManager.transaction.isActive).isFalse
     }
 
 }
